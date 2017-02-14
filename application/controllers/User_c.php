0ea131a48c1498a7c9d276dc2440ef0a13ca9879
alvinsanchez <sanchezalvin14@gmail.com>
2017-02-14 19:31:14 +0800

    asd

--- application/controllers/User_c.php
+++ application/controllers/User_c.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class User_c extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('login');
 	}
 }
