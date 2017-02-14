770b07d47523eefbff77ce1089c71cb1b82647cc
alvinsanchez <sanchezalvin14@gmail.com>
2017-02-14 19:50:51 +0800

    webconfig

--- application/controllers/User_c.php
+++ application/controllers/User_c.php
@@ -4,6 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class User_c extends CI_Controller {
 	public function index()
 	{
+		$this->load->view('header');
 		$this->load->view('login');
+		$this->load->view('footer');
 	}
 }
