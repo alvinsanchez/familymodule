72e266138ffd69a532b4154972566a2a905eb3db
alvinsanchez <sanchezalvin14@gmail.com>
2017-02-15 15:06:59 +0800

    asd

--- application/views/login.php
+++ application/views/login.php
@@ -1,32 +1,7 @@
-<<<<<<< HEAD
-<div class="container">
-  <div class="row col-md-5 col-md-offset-3" style="border:thin solid #ccc;padding: 2%;  margin-top: 5%; border-radius:4px; background-color:#FAFAFA;" >
-    <form class="form-horizontal" method="post" action="">
-  <div class="form-group">
-    <label for="Username" class="col-sm-2 control-label">Username</label>
-    <div class="col-sm-10">
-      <input type="username" class="form-control" id="username" name="username" placeholder="Username">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="Password" class="col-sm-2 control-label">Password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-      <button type="submit" id="submit" class="btn btn-primary form-control">Sign in</button>
-    </div>
-  </div>
-</form>
-  </div>
-</div>
-=======
 <div class="row">
   <div class="col-lg-4"></div>
   <div class="col-lg-4">
-    
+
     <div class="login">
       <div class="formholder">
         <div class="randompad">
@@ -36,7 +11,7 @@
               <input type="text" class="form-control" style="text-align: center;" name="username" id="user">
               <label for="inputPassword" style="font-weight: bolder;">Password:</label>
               <input type="password" class="form-control" style="text-align: center;" name="password" id="pass"><br />
-              <div class="alert-danger" align="center" id="errors">  
+              <div class="alert-danger" align="center" id="errors">
               </div>
              <button type="button" class="btn btn-success pull-right"> Login </button>
            </fieldset>
@@ -47,4 +22,3 @@
   </div>
   <div class="col-lg-4"></div>
 </div>
->>>>>>> 286c01860afda3f4fded7c6dff21c6541782c668
