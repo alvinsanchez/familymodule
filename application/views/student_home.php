28ea79e994c7c60f1936dd3ba22c8eb97021309e
alvinsanchez <sanchezalvin14@gmail.com>
2017-02-15 16:01:59 +0800

    asd

--- application/views/student_home.php
+++ application/views/student_home.php
@@ -51,62 +51,8 @@
             <div class="container-fluid">
     <section class="container">
         <div class="container-page" id="conset">
-            <div class="col-md-8">
-                <h4 class="dark-grey">User Information</h4>
-                <div class="form-group col-lg-6">
-                    <label>First Name</label>
-                    <input type="" name="fname" class="form-control" id="fname" value="">
-                </div>
-                <div class="form-group col-lg-6">
-                    <label>Last Name</label>
-                    <input type="" name="lname" class="form-control" id="lname" value="">
-                </div>
-                <div class="form-group col-lg-12">
-                    <label>Username</label>
-                    <input type="" name="user" class="form-control" id="user" value="">
-                </div>
-
-                <div class="form-group col-lg-6">
-                    <label>Password</label>
-                    <input type="password" name="pass" class="form-control" id="pass" value="">
-                </div>
-
-                <div class="form-group col-lg-6">
-                    <label>Confirm Password</label>
-                    <input type="password" name="conpass" class="form-control" id="conpass" value="">
-                </div>
-
-                <div class="form-group col-lg-6">
-                    <label>Email Address</label>
-                    <input type="" name="email" class="form-control" id="email" value="">
-                </div>
-            </div>
-            <div class="col-md-8">
-                <h4 class="dark-grey">Department</h4>
-                <div class="form-group col-lg-6">
-                <label style="font-weight: bolder;">Select Department</label>
-                <select id="selCat" class="form-control">
-                <option value="0" selected hidden>Select Category</option>
-                        <option value="1">Academic</option>
-                        <option value="2">Non Academic</option>
-                        <option value="3">Technical</option>
-                </select>
-                </div>
-                <div class="form-group col-lg-6">
-                    <label style="font-weight: bolder;">Select Department</label>
-                    <select class="form-control selDept" id="dept" name="selDept">
-                    </select>
-                </div>
-              <div style="display: none;" class="alert alert-success form-group col-lg-12"></div>
-                <div>
-                <div class="form-group col-lg-12">
-                    <button type="submit" class="btn btn-primary pull-right register" id="register">Register</button>
-                </div>
-            </div>
 
 
-            <!--  -->
-            </div>
          </section>
         </div>
         </div>
