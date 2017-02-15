42e474e060923cfac0d9b7d4517b65fd0dc2dbcd
nsgmng03 <nsgmng03@gmail.com>
2017-02-15 15:15:36 +0800

    asd

--- application/views/header.php
+++ application/views/header.php
@@ -14,24 +14,5 @@
 
 <body>
 
-<div class="navbar navbar-default">
-	<div class="container">
-	<a class="navbar-brand" href="index" style="color: #FFF;"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp;&nbsp;&nbsp;School Organization Management System</a>
-	<?php
-		if(isset($this->session->userdata['logged_in'])){
-      	?>
-				<ul class="nav navbar-nav navbar-right">
-			 <li class="dropdown">
-				 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"  style="color: #FFF;"><span class="glyphicon glyphicon-cog"></span></a>
-				 <ul class="dropdown-menu">
-					 <li><a href="#">Account Settings</a></li>
-					 <li><a href="<?php echo base_url() ?>admin/Admin/logout">Logout</a></li>
-				 </ul>
-				<?php
-      }
-			?>
-	 </li>
- </ul>
-	</div>
-</div>
+
 <div class="container-fluid">
